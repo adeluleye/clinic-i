e243f32c625ec3f21debc16eabe7f3026f380724
Ogamedia <kwame.yeboah@meltwater.org>
2015-03-04 09:20:31 +0000

    addded id to modal, not fully functional

--- clinic-i/client/helpers/events.js
+++ clinic-i/client/helpers/events.js
@@ -1,6 +1,15 @@
 Template.header.events({
 	'click #services': function () {
-		Check.insert({name: 'doro'});
-		console.log('run');
+		Check.insert({},function(e,r){
+			var checkin = Check.findOne(r);
+			id = checkin._id
+			console.log(id);
+			return id;
+		});
 	}
 });
+
+
+// Template.registerHelper('checker',function(){
+//   return Check.find({_id}, {sort: -1})
+// });
